e059ba992fee301d4313cf4ff18f8048b960fc86
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-05 21:39:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const tabs = document.querySelectorAll(".menu a");
 const sections = document.querySelectorAll(".tab");
-const menu = document.querySelector(".menu");
-const toggle = document.querySelector(".menu-toggle");
-
-toggle.onclick = () => menu.classList.toggle("show");
 
 tabs.forEach(tab => {
-  tab.onclick = () => {
+  tab.addEventListener("click", () => {
     tabs.forEach(t => t.classList.remove("active"));
     sections.forEach(s => s.classList.remove("active"));
 
     tab.classList.add("active");
     document.getElementById(tab.dataset.tab).classList.add("active");
-    menu.classList.remove("show");
-  };
+  });
 });
